1a909c9fa40753ad66218755173a1d5b8b0e481b
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2021-10-13 22:18:56 -0700

    display array data

--- assets/js/artorsaki/project-overview.js
+++ assets/js/artorsaki/project-overview.js
@@ -3,20 +3,24 @@ function main() {
         const data = JSON.parse(localStorage.getItem('projectData'));
 
         document.getElementById('project-status').innerText = data.projectStatus
-        document.getElementById('project-location').innerText = data.projectLocation
+        document.getElementById('project-location').innerText = joinArray(data.locations)
 
         // TODO, complex creator
         document.getElementById('project-links').innerText
 
-        // TODO, array and error catch if none
-        document.getElementById('tools').innerText
-
-        // TODO, array and error catch if none
-        document.getElementById('technologies').innerText
-
+        document.getElementById('tools').innerText = joinArray(data.tools)
+        document.getElementById('technologies').innerText = joinArray(data.technologies)
         document.getElementById('project-description').innerText = data.projectDescription
         
     })
+}
+
+function joinArray(arr) {
+    if (arr) {
+        return arr.join(', ')
+    } else {
+        return ''
+    }
 }
 
 main()
--- assets/js/project-form/project-form-test.js
+++ assets/js/project-form/project-form-test.js
@@ -1,4 +1,5 @@
 // TODO, remove this file from project-form.html to remove tests
+// TODO, a way to perform tests on load, and then once tests are done, reverse all actions
 
 document.addEventListener("DOMContentLoaded", function () {
     loadDummyData()
@@ -11,8 +12,8 @@ function loadDummyData() {
     addValue('slack-url', 'https://slack.com/');
     addValue('website-url', 'https://aveline-art.github.io/studious-lamp/');
     addValue('wiki-url', 'https://github.com/Aveline-art/studious-lamp/wiki');
-    addValue('technologies', 'django\nreact\nfigma');
-    addValue('tools', 'wooden rods\nrope\npulleys');
+    addValue('technologies', 'Django\nReact\nFigma');
+    addValue('tools', 'Wooden Rods\nRope\nPulleys');
     addValue('locations', 'Hippo\nMajordomo\nBestia');
 }
 
