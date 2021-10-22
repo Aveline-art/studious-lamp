c5ae4d802a7e74f6d3b8b89b3c0017ce36f51ac2
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2021-10-22 13:12:58 -0700

    created new helper

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,3 +1,34 @@
+function createDomObject(tag, attributes) {
+    const item = document.createElement(tag);
+    for (const prop in attributes) {
+        item.setAttribute(prop, attributes[prop]);
+    }
+    return item
+}
+
+function createNestedState(dict, dictRunner, key, init = {}) {
+    dictRunner[key] = []
+    
+    const handler = {
+        get: function (obj, prop) {
+            return obj[prop]
+        },
+        set: function (obj, prop, value) {
+            obj[prop] = value;
+            if (key in dictRunner) {
+                for (const f of dictRunner[key]) {
+                    f(dict[key]);
+                }
+            } else {
+                dictRunner[prop] = []
+            }
+            return true;
+        }
+    }
+
+    dict[key] = new Proxy(init, handler)
+}
+
 function createState(init = {}) {
     const runner = {}
     const handler = {
@@ -29,27 +60,29 @@ function createState(init = {}) {
     return [new Proxy(init, handler), runner]
 }
 
-function createNestedState(dict, dictRunner, key, init = {}) {
-    dictRunner[key] = []
-    
-    const handler = {
-        get: function (obj, prop) {
-            return obj[prop]
-        },
-        set: function (obj, prop, value) {
-            obj[prop] = value;
-            if (key in dictRunner) {
-                for (const f of dictRunner[key]) {
-                    f(dict[key]);
-                }
-            } else {
-                dictRunner[prop] = []
-            }
-            return true;
-        }
+function deep(object) {
+    return JSON.parse(JSON.stringify(object))
+}
+
+
+function loremIpsum(text, len) {
+    const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+    if (text && text.length > 1) {
+        return text
+    } else {
+        return helper(text, len)
     }
 
-    dict[key] = new Proxy(init, handler)
+    function helper(text, len) {
+        if (typeof text == 'string') {
+            return lorem.substring(0, len)
+        } else if (Array.isArray(text)) {
+            return lorem.split(' ').slice(0, len)
+        } else {
+            return lorem.split(' ').slice(0, len)
+        }
+    }
 }
 
 function toogleSeries(seriesId, targetId) {
@@ -62,18 +95,6 @@ function toogleSeries(seriesId, targetId) {
     target.removeAttribute('hidden');
 }
 
-function createDomObject(tag, attributes) {
-    const item = document.createElement(tag);
-    for (const prop in attributes) {
-        item.setAttribute(prop, attributes[prop]);
-    }
-    return item
-}
-
-function deep(object) {
-    return JSON.parse(JSON.stringify(object))
-}
 
 
-
-export { createState, createNestedState, toogleSeries, createDomObject, deep };
+export { createDomObject, createNestedState, createState, deep, loremIpsum, toogleSeries };
