df94daa94ba2100e6e9cdf2daf05fd79b789d82c
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2021-10-27 09:59:40 -0700

    testing github action to reformat issue

---
+++ .github/workflows/github-actions/trigger-issue/reformat-project-card-update-issue/edit-top-comment.js
@@ -0,0 +1,45 @@
+// Import modules
+var fs = require("fs");
+
+// Global variables
+var github;
+var context;
+
+/**
+ * Formats the commandline instructions into a template, then posts it to the pull request.
+ * @param {Object} g - github object  
+ * @param {Object} c - context object 
+ */
+async function main({ g, c }) {
+    github = g;
+    context = c;
+
+    const title = github.event.issue.title
+    const body = github.event.issue.body
+    const issueNum = context.payload.number 
+
+    const newIssueComment = formatComment(title, body)
+    postComment(issueNum, newIssueComment);
+}
+
+function formatComment(title, body) {
+    const path = '/project-card-comment.md'
+    const text = fs.readFileSync(path).toString('utf-8');
+    const completedInstuctions = text.replace('${title}', title).replace('${body}', body)
+    return completedInstuctions
+}
+
+async function postComment(issueNum, instructions) {
+    try {
+        await github.issues.update({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: issueNum,
+            body: instructions,
+        });
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+module.exports = main
---
+++ .github/workflows/github-actions/trigger-issue/reformat-project-card-update-issue/project-card-comment.md
@@ -0,0 +1,20 @@
+### Overview
+
+As a developer on the website, we have to make sure that the projects of our organization is up to date for our visitors. For this issue we will update the project card for ${title}
+
+### Action Items
+
+- [ ] Locate the project's markdown file, if it exists. Otherwise, create it.
+- [ ] Verify that all required fields are there.
+- [ ] Replace the file with the markdown located in the Instructions/Resources section below.
+
+### Instructions/Resources
+
+<details>
+<summary>New markdown for ${title}</summary>
+<br>
+<pre>
+${body}
+</pre>
+<br>
+</details>
